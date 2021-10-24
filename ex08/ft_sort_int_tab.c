8c95bae7637273549e301d3c7c39cd769a71493a
Jinust <hyeongjinh@naver.com>
2021-10-24 19:34:51 +0900

    go hmoe

---
+++ ex08/ft_sort_int_tab.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyehwang <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/24 17:14:53 by hyehwang          #+#    #+#             */
+/*   Updated: 2021/10/24 19:29:18 by hyehwang         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+#include<stdio.h>
+
+
+void ft_sort_int_tab(int *tab,int size){
+	printf("START\n\n\n");
+	
+	int count =0;
+	int n = 0;
+	while(1){
+		int tmp;	
+		if(tab[n]>tab[n+1])
+		{ 
+			printf("check!!");
+			tmp = tab[n];
+		   tab[n] = tab[n+1];
+		   tab[n+1] = tmp;
+		   count = 0 ;
+				  
+			if((n+1)>=(size-1)){
+				n = 0;
+			}
+		}
+		else{
+			printf("Good\n");
+			n++;
+			count ++;
+			int i = 0;
+			if(count >= (size-1)){
+				printf("plz\n\n");
+				while(i<=4){printf("tab[%d] : %d \n",i,tab[i]),i++;
+					
+		}
+				break;
+	}
+	}
+	}}	
+int main(){
+	int tt[5]={5,4,3,1,2};
+	int *pp = tt;
+	ft_sort_int_tab(pp,5);
+
+}
